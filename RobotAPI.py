76d09fdad50f3a9e7ff36411e8489810f4598bd6
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2022-11-12 14:49:26 -0800

    Update RobotAPI.py

--- RobotAPI.py
+++ RobotAPI.py
@@ -1,1 +1,7 @@
 # Use this file to communicate with the RaspberryPi and Arduino
+from gpiozero import LED, Button
+from time import sleep
+from signal import pause
+
+def pingLED17():
+    return None
