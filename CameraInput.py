cae1f1883309d03da9086a6b4ab5a24479799cfb
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2022-11-20 21:43:34 -0800

    Imported face-tracking demo from OpenCVproj.py into CameraInput.py and made coordinate yielder
    used the Demo logic to create a Generator function "trackObject()" which YIELDs an (x,y) tuple of the Bounding Box' location.
    
    main.py also calls this new function and prints to terminal to give live-time coordinates of the Object of Interest.
    
    In later version, should switch out the faceCascade for a Shapes cascade

--- CameraInput.py
+++ CameraInput.py
@@ -28,3 +28,37 @@ def printTowelAlternating(i):
     #   (Window MUST be selected first)
     cv2.waitKey(0)
     print("Start Window closed")
+
+
+#run a FOREVER loop. shows camera feed and draws bounding box around
+# object of interest, and also YIELDS the x,y of the box to caller func
+def trackObject():
+    imcap = cv2.VideoCapture(0)
+
+    imcap.set(3, 640) # Set field 3 (width) to 640
+    imcap.set(4, 480) # Set field 4 (Height) 480
+
+    # import cascade (OpenCV pre-trained HAAR classifier)
+    faceCascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    while True:
+        success, img = imcap.read() # capture frame from video
+        # converting image from color to grayscale 
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Getting corners around the face
+        # 1.3 = scale factor, 5 = minimum neighbor can be detected
+        faces = faceCascade.detectMultiScale(imgGray, 1.3, 5)  
+
+        # drawing bounding box around face
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255,   0), 3)
+            yield x,y
+        # displaying image with bounding box
+        cv2.imshow('face_detect', img)
+        # loop will be broken when 'q' is pressed on the keyboard
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    imcap.release()
+    cv2.destroyWindow('face_detect')
--- main.py
+++ main.py
@@ -11,37 +11,37 @@ print("Running main.py")
 
 CameraInput.testprint()
 
-i = 0
-while (1):
-    i += 1
-    print(i)
-    CameraInput.printTowelAlternating(i)
+for i in CameraInput.trackObject():
+    print(i, end='')
+    if (i[0] >= 700):
+        print("Camera looking too far left! Must turn towards right...")
+    else:
+        print("Camera looking too far right! Must turn towards left...")
 
 
+import cv2
+import numpy as np
 
-# import cv2
-# import numpy as np
+large = cv2.imread('1.jpg')
 
-# large = cv2.imread('1.jpg')
+small = cv2.cvtColor(large, cv2.COLOR_BGR2GRAY)
 
-# small = cv2.cvtColor(large, cv2.COLOR_BGR2GRAY)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
 
-# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-# grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+_, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-# _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
 
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-# connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-# contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+mask = np.zeros(bw.shape, dtype=np.uint8)
 
-# mask = np.zeros(bw.shape, dtype=np.uint8)
-
-# with open("coords.txt","w+") as file:
-#     for idx in range(len(contours)):
-#         x, y, w, h = cv2.boundingRect(contours[idx])
-#         mask[y:y+h, x:x+w] = 0
-#         file.write("Box {0}: ({1},{2}), ({3},{4}), ({5},{6}), ({7},{8})".format(idx,x,y,x+w,y,x+w,y+h,x,y+h))
-#         cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
-#         r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+with open("coords.txt","w+") as file:
+    for idx in range(len(contours)):
+        x, y, w, h = cv2.boundingRect(contours[idx])
+        mask[y:y+h, x:x+w] = 0
+        file.write("Box {0}: ({1},{2}), ({3},{4}), ({5},{6}), ({7},{8})".format(idx,x,y,x+w,y,x+w,y+h,x,y+h))
+        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+        r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
